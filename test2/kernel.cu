1303209d9c0d7040ee321937772790fc921ece92
Con Vu Outlook Acc <vutrancon@outlook.com>
2018-12-05 18:42:16 +0100

    basic quadric intersection in tester kernel

--- test2/kernel.cu
+++ test2/kernel.cu
@@ -54,13 +54,13 @@ public:
 	T diameter; // default to 10 mm, see constructor
 	int type; //0 is image surface, 1 is power surface, 2 is stop surface
 
-	mysurface(const vec3<T>& pos = vec3<T>(0,0,0), T diameter = 10, int type = 0) :
+	__host__ __device__ mysurface(const vec3<T>& pos = vec3<T>(0,0,0), T diameter = 10, int type = 0) :
 		pos(pos), diameter(diameter), type(type)
 	{
 		LOG1("my surface created")
 	}
 
-	~mysurface()
+	__host__ __device__ ~mysurface()
 	{
 		LOG1("surface destructor called")
 	}
@@ -78,13 +78,13 @@ class powersurface:public mysurface<T>
 public:
 	T power;//just random number, default to 0.1 mm^-1
 
-	powersurface(T power = 0.1, const vec3<T>& pos = vec3<T>(0, 0, 0), T diameter = 10) :
+	__host__ __device__ powersurface(T power = 0.1, const vec3<T>& pos = vec3<T>(0, 0, 0), T diameter = 10) :
 		mysurface(pos, diameter, 1), power(power)
 	{
 		LOG1("power surface created")
 	}
 
-	~powersurface()
+	__host__ __device__ ~powersurface()
 	{
 		LOG1("power surface destructor called")
 	}
@@ -95,7 +95,44 @@ public:
 	}
 };
 
+template<typename T = float>
+class quadricparam
+{
+public:
+	T A, B, C, D, E, F, G, H, I, J; //implicit equation A*x^2+B*y^2+C*z^2+D*x*y+E*y*z+F*x*z+G*x+H*y+I*z+J = 0
 
+	__host__ __device__ quadricparam(T A = 1, T B = 1, T C = 1, T D = 0, T E = 0, T F = 0, T G = 0, T H = 0, T I = 0, T J = 0) :
+		A(A), B(B), C(C), D(D), E(E), F(F), G(G), H(H), I(I), J(J)
+	{}
+};
+
+template<typename T = float>
+class quadricsurface :public mysurface<T>
+{
+public:
+	quadricparam<T> param;
+	T n1, n2;
+
+	__host__ __device__ quadricsurface(const quadricparam<T>& param = quadricparam<T>(1,1,1,0,0,0,0,0,0,-1),
+		T n1 = 1, T n2 = 1, const vec3<T>& pos = vec3<T>(0, 0, 0), T diameter = 10):
+		mysurface(pos, diameter, 1), param(param), n1(n1), n2(n2)
+	{
+		LOG1("quadric surface created")
+	}
+
+	__host__ __device__ ~quadricsurface()
+	{
+		LOG1("quadric surface destructor called")
+	}
+
+	int size()
+	{
+		return sizeof(*this);
+	}
+};
+
+//main tracing kernel
+#ifdef nothing
 template <typename T = float>
 __global__ void tracer(raysegment<T>* inbundle, raysegment<T>* outbundle, const mysurface<T>* nextsurface)
 {
@@ -165,24 +202,59 @@ __global__ void tracer(raysegment<T>* inbundle, raysegment<T>* outbundle, const 
 	/*printf("%d at t = %f at dir (%f,%f,%f), after dir (%f,%f,%f)\n", idx, t, at.dir.x, at.dir.y, 
 		at.dir.z, after.dir.x, after.dir.y, after.dir.z );*/
 }
+#endif
 
+//tester kernel
 __global__ void tester()
 {
-#ifdef nothing
-	vec3<MYFLOATTYPE> lhs(sqrtf((float)2), sqrtf((float)2), 0);
-	vec3<MYFLOATTYPE> rhs(-sqrtf((float)2), sqrtf((float)2), 0);
-	auto result1 = lhs + rhs;
-	auto result2 = lhs - rhs;
-	auto result3 = dot(lhs, rhs);
-	auto result4 = cross(lhs, rhs);
-	auto result5 = norm(lhs);
-	printf("(%f,%f,%f) (%f,%f,%f) %f (%f,%f,%f) %f", result1.x, result1.y, result1.z, result2.x, result2.y, result2.z,
-		result3, result4.x, result4.y, result4.z, result5);
-#endif
-	float a = 5;
-	float b = 6;
-	float c = a / b;
-	printf("%f", c);
+	auto pquad = new quadricsurface<MYFLOATTYPE>(quadricparam<MYFLOATTYPE>(1,1,1,0,0,0,0,0,0,-1));
+	auto pray = new raysegment<MYFLOATTYPE>(vec3<MYFLOATTYPE>(0, 0, 0), vec3<MYFLOATTYPE>(0, 0, 1));
+	MYFLOATTYPE &A = pquad->param.A,
+		&B = pquad->param.B,
+		&C = pquad->param.C,
+		&D = pquad->param.D,
+		&E = pquad->param.E,
+		&F = pquad->param.F,
+		&G = pquad->param.G,
+		&H = pquad->param.H,
+		&K = pquad->param.I, // in order not to mix with imaginary unit, due to the symbolic calculation in Maple
+		&J = pquad->param.J;
+	MYFLOATTYPE &p1 = pray->pos.x,
+		&p2 = pray->pos.y,
+		&p3 = pray->pos.z,
+		&d1 = pray->dir.x,
+		&d2 = pray->dir.y,
+		&d3 = pray->dir.z;
+	MYFLOATTYPE t1,t2;
+	MYFLOATTYPE delta = - 4*A*B*d1*d1*p2*p2 + 8*A*B*d1*d2*p1*p2 - 4*A*B*d2*d2*p1*p1 
+		- 4*A*C*d1*d1*p3*p3 + 8 * A*C*d1*d3*p1*p3 - 4 * A*C*d3*d3*p1*p1 - 4*A*F*d1*d1*p2*p3 
+		+ 4*A*F*d1*d2*p1*p3 + 4*A*F*d1*d3*p1*p2 - 4*A*F*d2*d3*p1*p1 - 4 * B*C*d2*d2*p3*p3 
+		+ 8*B*C*d2*d3*p2*p3 - 4*B*C*d3*d2*p2*p2 + 4*B*E*d1*d2*p2*p3 - 4*B*E*d1*d3*p2*p2 
+		- 4*B*E*d2*d2*p1*p3 + 4*B*E*d2*d3*p1*p2 - 4*C*D*d1*d2*p3*p3 + 4*C*D*d1*d3*p2*p3 
+		+ 4*C*D*d2*d3*p1*p3 - 4*C*D*d3*d3*p1*p2 + D*D*d1*d1*p2*p2 - 2*D*D*d1*d2*p1*p2 
+		+ D*D*d2*d2*p1*p1 + 2*D*E*d1*d1*p2*p3 - 2*D*E*d1*d2*p1*p3 - 2*D*E*d1*d3*p1*p2 
+		+ 2*D*E*d2*d3*p1*p1 - 2*D*F*d1*d2*p2*p3 + 2*D*F*d1*d3*p2*p2 + 2*D*F*d2*d2*p1*p3 
+		- 2*D*F*d2*d3*p1*p2 + E*E*d1*d1*p3*p3 - 2*E*E*d1*d3*p1*p3 + E*E*d3*d3*p1*p1 
+		+ 2*E*F*d1*d2*p3*p3 - 2*E*F*d1*d3*p2*p3 - 2*E*F*d2*d3*p1*p3 + 2*E*F*d3*d3*p1*p2 
+		+ F*F*d2*d2*p3*p3 - 2*F*F*d2*d3*p2*p3 + F*F*d3*d3*p2*p2 - 4*A*H*d1*d1*p2 
+		+ 4*A*H*d1*d2*p1 - 4*A*K*d1*d1*p3 + 4*A*K*d1*d3*p1 + 4*B*G*d1*d2*p2 - 4*B*G*d2*d2*p1 
+		- 4*B*K*d2*d2*p3 + 4*B*K*d2*d3*p2 + 4*C*G*d1*d3*p3 - 4*C*G*d3*d3*p1 + 4*C*H*d2*d3*p3 
+		- 4*C*H*d3*d3*p2 + 2*D*G*d1*d1*p2 - 2*D*G*d1*d2*p1 - 2*D*H*d1*d2*p2 
+		+ 2*D*H*d2*d2*p1 - 4*D*K*d1*d2*p3 + 2*D*K*d1*d3*p2 + 2*D*K*d2*d3*p1 + 2*E*G*d1*d1*p3 
+		- 2*E*G*d1*d3*p1 + 2*E*H*d1*d2*p3 - 4*E*H*d1*d3*p2 + 2*E*H*d2*d3*p1 - 2*E*K*d1*d3*p3 
+		+ 2*E*K*d3*d3*p1 + 2*F*G*d1*d2*p3 + 2*F*G*d1*d3*p2 - 4*F*G*d2*d3*p1 + 2*F*H*d2*d2*p3 
+		- 2*F*H*d2*d3*p2 - 2*F*K*d2*d3*p3 + 2*F*K*d3*d3*p2 - 4*A*J*d1*d1 - 4*B*J*d2*d2 
+		- 4*C*J*d3*d3 - 4*D*J*d1*d2 - 4*E*J*d1*d3 - 4*F*J*d2*d3 + G*G*d1*d1 + 2*G*H*d1*d2 
+		+ 2*G*K*d1*d3 + H*H*d2*d2+ 2*H*K*d2*d3 + K*K*d3*d3;
+	MYFLOATTYPE deno = -2 * (A*d1*d1 + B*d2*d2 + C*d3*d3 + D*d1*d2 + E*d1*d3 + F*d2*d3);
+	MYFLOATTYPE beforedelta = 2 * A*d1*p1 + 2 * B*d2*p2 + 2 * C*d3*p3 + D * (d1*p2 + d2 * p1) + E * (d1*p3 + d3 * p1) + F * (d2*p3 + d3 * p2) + G * d1 + H * d2 + K * d3;
+	t1 = (delta >= 0) ? (beforedelta + sqrtf(delta)) / deno : MYINFINITY + 1;
+	t2 = (delta >= 0) ? (beforedelta - sqrtf(delta)) / deno : MYINFINITY + 1;
+
+	printf("delta = %f ,beforedelta = %f ,deno = %f", delta, beforedelta, deno);
+	printf("t1 = %f ,t2 = %f \n", t1, t2);
+
+	delete pquad;
 }
 
 #ifdef nothing
@@ -208,13 +280,37 @@ public:
 int main()
 {
 	LOG1("this is main program");
-
-
+#ifdef something
 	//create event for timing
 	cudaEvent_t start, stop;
 	CUDARUN(cudaEventCreate(&start));
 	CUDARUN(cudaEventCreate(&stop));
 
+	//start timing 
+	CUDARUN(cudaEventRecord(start, 0));
+
+	//launch kernel
+	{tester << <1, 1 >> > ();
+	cudaError_t cudaStatus = cudaGetLastError();
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "Error at file %s line %d, ", __FILE__, __LINE__);
+		fprintf(stderr, "code %d, reason %s\n", cudaStatus, cudaGetErrorString(cudaStatus));
+	}
+	}
+
+	//kernel finished, stop timing, print out elapsed time
+	CUDARUN(cudaEventRecord(stop, 0));
+	CUDARUN(cudaEventSynchronize(stop));
+	float elapsedtime;
+	CUDARUN(cudaEventElapsedTime(&elapsedtime, start, stop));
+	LOG2("kernel run time: " << elapsedtime << " ms\n");
+#endif
+
+#ifdef nothing
+	//create event for timing
+	cudaEvent_t start, stop;
+	CUDARUN(cudaEventCreate(&start));
+	CUDARUN(cudaEventCreate(&stop));
 
 	//set up the surfaces manually !!!!
 	float diam = 10;
@@ -290,16 +386,6 @@ int main()
 	}
 
 
-#ifdef nothing
-	{tester << <1, 1 >> > (); 
-		cudaError_t cudaStatus = cudaGetLastError(); 
-		if (cudaStatus != cudaSuccess) {
-				fprintf(stderr, "Error at file %s line %d, ", __FILE__, __LINE__); 
-				fprintf(stderr, "code %d, reason %s\n", cudaStatus, cudaGetErrorString(cudaStatus)); 
-		}
-	}
-#endif
-
 	//kernel finished, stop timing, print out elapsed time
 	CUDARUN(cudaEventRecord(stop, 0));
 	CUDARUN(cudaEventSynchronize(stop));
@@ -356,4 +442,5 @@ int main()
 	}
 
 	delete[] bundles;
+#endif
 }
